342959e2506bc4a7db302e6508f3352eb32d12d1
rserran <rserran1@gmail.com>
2023-09-17 16:06:47 -0500

    Add input type check to 'sizes' argument, update test.

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -7,10 +7,25 @@
 #' @export
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
+     if(!is.logical(sizes)) {
+          stop("'sizes' must be TRUE or FALSE.")
+     }
+
      pkgs <- utils::installed.packages()
      pkg_tbl <- table(pkgs[, "LibPath"])
      pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
      names(pkg_df) <- c("Library", "n_packages")
+
+     if (sizes) {
+
+          pkg_df$lib_size <- sapply(
+               pkg_df$Library,
+               function(x) {
+                    sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+               }
+          )
+     }
+
      pkg_df
 }
